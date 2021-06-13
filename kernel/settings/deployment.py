c39c54b3e808a486d3ad13e9ecf7ca667ee52c60
mehran rahmanzadeh <mehran@theivan.org>
2021-06-13 16:02:06 +0430

    refa (project): Refactor docs and kernel
    - remove docs
    - remove kernel (we dont need in production)

--- kernel/settings/deployment.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-from .secure import *
-from .packages import *
-
-from decouple import config
-
-DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
