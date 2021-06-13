5612dcfecb6ae95920ac46b48bf83840d500f2eb
Sepehr <sa.goldeneagle@gmail.com>
2021-06-13 18:02:26 +0430

    fix (pypi): Fix pypi config files

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name = 'django-sage-painless',         # How you named your package folder (MyLib)
     packages = ['sage_painless'],   # Chose the same as "name"
-    version = '0.2',      # Start with a small number and increase it with every change you make
+    version = '0.1.1',      # Start with a small number and increase it with every change you make
     license='GNU',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'django package for auto generating projects',   # Give a short description about your library
     author = 'Sage Team',                   # Type in your name
     author_email = 'mail@sageteam.org',      # Type in your E-Mail
     url = 'https://github.com/sageteam-org/django-sage-painless',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/sageteam-org/django-sage-painless/archive/refs/tags/0.2.tar.gz',    # I explain this later on
+    download_url = 'https://github.com/sageteam-org/django-sage-painless/archive/refs/tags/0.3.tar.gz',    # I explain this later on
     keywords = ['django', 'python', 'generate', 'code generator'],   # Keywords that define your package best
     install_requires=[
         'Django',
