df044cc704ac5513fa4e124e15d4d420f2529bbe
Sepehr <sa.goldeneagle@gmail.com>
2021-06-13 17:59:04 +0430

    fix (pypi): Fix setup files

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
     name = 'django-sage-painless',         # How you named your package folder (MyLib)
     packages = ['sage_painless'],   # Chose the same as "name"
-    version = '0.1',      # Start with a small number and increase it with every change you make
+    version = '0.2',      # Start with a small number and increase it with every change you make
     license='GNU',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'django package for auto generating projects',   # Give a short description about your library
     author = 'Sage Team',                   # Type in your name
@@ -14,5 +11,12 @@ setup(
     url = 'https://github.com/sageteam-org/django-sage-painless',   # Provide either the link to your github or to your website
     download_url = 'https://github.com/sageteam-org/django-sage-painless/archive/refs/tags/0.2.tar.gz',    # I explain this later on
     keywords = ['django', 'python', 'generate', 'code generator'],   # Keywords that define your package best
-    install_requires=required
+    install_requires=[
+        'Django',
+        'django-redis',
+        'redis',
+        'drf-yasg',
+        'django-seed',
+        'Faker'
+    ]
 )
