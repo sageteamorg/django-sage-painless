68db7305e43a35dc2125d55e290e2074079c3da2
mehran rahmanzadeh <mehran@theivan.org>
2021-06-29 13:10:18 +0430

    fix (pypi): Fix pypi config files

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-sage-painless',         # How you named your package folder (MyLib)
-    packages = find_packages(exclude=['tests*']),   # Chose the same as "name"
-    version = '0.0.3',      # Start with a small number and increase it with every change you make
-    license='GNU',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description = 'django package for auto generating projects',   # Give a short description about your library
-    author = 'Sage Team',                   # Type in your name
-    author_email = 'mail@sageteam.org',      # Type in your E-Mail
-    url = 'https://github.com/sageteam-org/django-sage-painless',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/sageteam-org/django-sage-painless/archive/refs/tags/0.3.tar.gz',    # I explain this later on
-    keywords = ['django', 'python', 'generate', 'code generator'],   # Keywords that define your package best
+    name='django-sage-painless',
+    packages=find_packages(exclude=['tests*']),
+    package_data={'sage_painless/templates': ['*.txt']},
+    include_package_data=True,
+    version='0.0.8',
+    license='GNU',
+    description='django package for auto generating projects',
+    author='Sage Team',
+    author_email='mail.sageteam.org',
+    url='https://github.com/sageteam-org/django-sage-painless',
+    download_url='https://github.com/sageteam-org/django-sage-painless/archive/refs/tags/0.4.tar.gz',
+    keywords=['django', 'python', 'generate', 'code generator'],
     install_requires=[
         'Django',
         'django-redis',
         'redis',
         'drf-yasg',
         'django-seed',
-        'Faker'
+        'Faker',
+        'autopep8'
     ]
 )
