869f0aba45ca998e65223ef927281342e4a22ad4
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-07-15 11:36:16 +0430

    refa (sage_painless--utils): Change pep8 service
    - use autopep8 module
    - select apply options

--- sage_painless/utils/pep8_service.py
+++ sage_painless/utils/pep8_service.py
@@ -1,18 +1,11 @@
-import subprocess
-
+import autopep8
 
 class Pep8:
     def fix_pep8(self, file_path):
         """
-        fix pep8
+        fix pep8 (E122, E303, E305, W292, W391)
         """
-        subprocess.call(
-            [
-                'autopep8',
-                '--in-place',
-                '--aggressive',
-                '--aggressive',
-                file_path
-            ],
-            shell=True
+        options = autopep8.parse_args(
+            ['--in-place', '--aggressive', '--select', 'E122,E271,E261,E303,E305,W292,W391', file_path]
         )
+        autopep8.fix_file(filename=file_path, options=options)
