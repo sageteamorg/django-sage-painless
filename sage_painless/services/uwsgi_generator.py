419f63ff8cc54c6b4c0791bfc9c3f5f2672815c6
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-08-13 23:36:48 +0430

    feat (sage_painless--services): Add git support to uwsgi generator

--- sage_painless/services/uwsgi_generator.py
+++ sage_painless/services/uwsgi_generator.py
@@ -4,12 +4,13 @@ import time
 from django.conf import settings
 
 from sage_painless import templates
+from sage_painless.utils.git_service import GitSupport
 from sage_painless.utils.jinja_service import JinjaHandler
 from sage_painless.utils.json_service import JsonHandler
 from sage_painless.utils.timing_service import TimingService
 
 
-class UwsgiGenerator(JinjaHandler, JsonHandler, TimingService):
+class UwsgiGenerator(JinjaHandler, JsonHandler, TimingService, GitSupport):
     """generate uwsgi config"""
     DEPLOY_KEYWORD = 'deploy'
     UWSGI_KEYWORD = 'uwsgi'
@@ -24,7 +25,7 @@ class UwsgiGenerator(JinjaHandler, JsonHandler, TimingService):
             raise KeyError('`deploy` not set in diagram json file')
         return deploy.get(self.UWSGI_KEYWORD)
 
-    def generate(self, diagram_path):
+    def generate(self, diagram_path, git_support=False):
         """generate uwsgi.ini
         template:
             sage_painless/templates/uwsgi.txt
@@ -35,6 +36,9 @@ class UwsgiGenerator(JinjaHandler, JsonHandler, TimingService):
 
         config = self.extract_uwsgi_config(diagram)  # get uwsgi config from diagram
 
+        if git_support:
+            self.init_repo(settings.BASE_DIR)
+
         # generate uwsgi.ini
         self.stream_to_template(
             output_path=f'{settings.BASE_DIR}/uwsgi.ini',
@@ -43,6 +47,12 @@ class UwsgiGenerator(JinjaHandler, JsonHandler, TimingService):
                 'config': config
             }
         )
+
+        if git_support:
+            self.commit_file(
+                f'{settings.BASE_DIR}/uwsgi.ini',
+                f'deploy (uwsgi): Create uwsgi config file'
+            )
 
         end_time = time.time()
         return True, 'uwsgi config generated ({:.3f} ms)'.format(self.calculate_execute_time(start_time, end_time))
