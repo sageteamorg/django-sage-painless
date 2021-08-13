d7bdf4a0e44b3d361add2e54750077e71036c4b1
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-08-13 16:41:45 +0000

    feat (products--signals): Create cache update signals

--- products/signals.py
+++ products/signals.py
@@ -15,25 +15,26 @@ from products.models.discount import Discount
 from products.services import clear_cache_for_model
 
 
+
+
 def category_clear_cache(sender, **kwargs):
     clear_cache_for_model(sender.CACHE_KEY)
-
-
 signals.post_save.connect(category_clear_cache, sender=Category)
 signals.pre_delete.connect(category_clear_cache, sender=Category)
 
 
+
 def product_clear_cache(sender, **kwargs):
     clear_cache_for_model(sender.CACHE_KEY)
-
-
 signals.post_save.connect(product_clear_cache, sender=Product)
 signals.pre_delete.connect(product_clear_cache, sender=Product)
 
 
+
 def discount_clear_cache(sender, **kwargs):
     clear_cache_for_model(sender.CACHE_KEY)
-
-
 signals.post_save.connect(discount_clear_cache, sender=Discount)
 signals.pre_delete.connect(discount_clear_cache, sender=Discount)
+
+
+
