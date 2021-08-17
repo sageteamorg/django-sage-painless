831ad3f9e5385b7d85ff61d3eec9847b33e930c3
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-08-18 00:19:22 +0430

    feat (sage_painless--validators): Add setting validator
    - validate encryption config

---
+++ sage_painless/validators/setting_validator.py
@@ -0,0 +1,16 @@
+from django.db import connection
+
+
+class SettingValidator:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def validate_pgcrypto_config(cls):
+        """validate database settings for encryption
+        django-sage-encrypt just works in PostgreSQL
+        check default db
+        """
+        default_vendor = connection.vendor
+        if not default_vendor == 'postgresql':
+            raise AssertionError('encryption just works on PostgreSQL.')
