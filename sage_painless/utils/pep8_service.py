bb2d097e973805fd986e0e68bdfd07a0b7ec6f24
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-11-03 17:44:05 +0330

    feat (sage_painless--utils): Add new rule to pep8 service

--- sage_painless/utils/pep8_service.py
+++ sage_painless/utils/pep8_service.py
@@ -10,9 +10,9 @@ class Pep8:
     @classmethod
     def fix_pep8(cls, file_path):
         """
-        fix pep8 (E122,E271,E261,E225,E303,E302,E305,E501,W292,W391)
+        fix pep8 (E122,E271,E231,E261,E225,E303,E302,E305,E501,W292,W391)
         """
         options = autopep8.parse_args(
-            ['--in-place', '--aggressive', '--select', 'E122,E271,E261,E225,E303,E302,E305,E501,W292,W391', file_path]
+            ['--in-place', '--aggressive', '--select', 'E122,E271,E231,E261,E225,E303,E302,E305,E501,W292,W391', file_path]
         )
         autopep8.fix_file(filename=file_path, options=options)
